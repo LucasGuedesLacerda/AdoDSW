4dc6a495c20db10e4f5a0022b434f98c3201ac89
LucasGuedesLacerda <lucasguedeslacerda@gmail.com>
2025-08-20 12:56:52 -0300

    Corrigido pom.xml e mapeamento do servlet

---
+++ carstore/src/main/java/com/carstore/servlet/CarroServlet.java
@@ -0,0 +1,28 @@
+package com.carstore.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/carro")
+public class CarroServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String json = "{ \"nome\": \"Fusca\", \"cor\": \"Azul\" }";
+
+        PrintWriter out = response.getWriter();
+        out.print(json);
+        out.flush();
+    }
+}
